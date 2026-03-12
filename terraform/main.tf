24907fe02c9a8aa309c5cc2abe4196d5cb5189b1
Jinal Mahyavanshi <95264718+jinalmahyavanshi@users.noreply.github.com>
2026-03-12 13:45:53 +0000

    Update Terraform config for pipeline execution

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,9 @@
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Secure web security group"
+terraform {
+  required_version = ">= 1.0"
+}
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    description = "SSH access"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    description = "App access"
-    cidr_blocks = ["10.0.0.0/16"]
+resource "null_resource" "devsecops_example" {
+  provisioner "local-exec" {
+    command = "echo DevSecOps Terraform stage executed successfully"
   }
 }
